8b4dfd1af532336fe36c5c641b9440b258d702dc
Abdallah <bratosab@gmail.com>
2024-12-04 10:20:55 +0100

    feat(ngrx): created app store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode,
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { appReducer } from './store/app.state';
 
 export const config: ApplicationConfig = {
-  providers: [provideHttpClient(), provideRouter([...appRoutes])],
+  providers: [
+    provideHttpClient(),
+    provideRouter([...appRoutes]),
+    provideStore({ router: routerReducer, app: appReducer }),
+    provideEffects(),
+    provideRouterStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 };
---
+++ src/app/store/app.state.ts
@@ -0,0 +1,25 @@
+import { createReducer, on } from "@ngrx/store";
+import { Status } from "../models/status.enum";
+import { SetName, SetStatus, SetTel } from "./app.actions";
+
+export interface AppState {
+    name: string;
+    tel: string;
+    status: Status | null;
+    orderId: number | null;
+  }
+
+  export const initialState: AppState = {
+    name: '',
+    tel: '',
+    status: null,
+    orderId: null,
+  };
+
+
+export const appReducer = createReducer(
+    initialState,
+    on(SetName, (state, { name }) => ({ ...state, name })),
+    on(SetTel, (state, { tel }) => ({ ...state, tel })),
+    on(SetStatus, (state, { status }) => ({ ...state, status }))
+  );
